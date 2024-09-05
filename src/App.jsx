b9f627f56bf6fc016f874ca82792494fbcc8ff4f
esteban slavitt <152658032+EstebanSlavitt@users.noreply.github.com>
2024-09-05 09:41:50 -0700

    initial commit for dnd frontend

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import { Header } from "./Header";
+import { DndPage } from "./DndPage";
+import { Footer } from "./Footer";
+
+function App() {
+  return (
+    <div>
+      <Header />
+      <DndPage />
+      <Footer />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/DndPage.jsx
@@ -0,0 +1,7 @@
+export function dndPage() {
+  return (
+    <main>
+      <h1>Welcome to React!</h1>
+    </main>
+  );
+}
